11ef01c241c9ae74bed3d1c7ed15b09e9e586d1b
chiragmalhotravbn <101737410+chiragmalhotravbn@users.noreply.github.com>
2023-03-02 13:21:26 +0530

    Update PrePostDeploymentScript.ps1

--- PrePostDeploymentScript.ps1
+++ PrePostDeploymentScript.ps1
@@ -5,11 +5,7 @@ param
     [parameter(Mandatory = $false)] [String] $DataFactoryName,
     [parameter(Mandatory = $false)] [Bool] $predeployment=$true,
     [parameter(Mandatory = $false)] [Bool] $deleteDeployment=$false,
-    [parameter(Mandatory = $false)] [string] $clientId,
-    [parameter(Mandatory = $false)] [string] $clientSecret,
-    [parameter(Mandatory = $false)] [string] $tenantId,
-    [parameter(Mandatory = $false)] [string] $subscriptionId  
+    [parameter(Mandatory = $false)] [string] $secrets
 )
 
-Write-output $clientId
-Write-output $clientSecret
+Write-output $secrets
